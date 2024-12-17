c24c5e2de5f19a53b7b791810d7a50f1220f7b1c
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-12-17 16:24:47 +0100

    Doc: install tailwind packages + configuration

---
+++ frontend/tailwind.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+
+# tailwind.config.js
+#   content: [
+#    "./index.html",
+#    "./src/**/*.tsx",
+#  ],
+#
+#  index.css
+#
+# @tailwind base;
+# @tailwind components;
+# @tailwind utilities;
+#
+# Package.json
+#
+# "tailwind": "npx tailwindcss -i ./src/stylesheets/index.css -o ./src/output.css --watch",
