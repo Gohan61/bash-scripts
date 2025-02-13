f517542235764cfa662cf7a88c843f6bed00df35
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2025-02-13 15:54:20 +0100

    Doc: update to use new tailwind spec + print Lines
    * Instead of opening file to read comments they are printed on console
    as a reminder

--- frontend/tailwind.sh
+++ frontend/tailwind.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-npm install -D tailwindcss postcss autoprefixer
-npx tailwindcss init -p
+npm install -D tailwindcss @tailwindcss/vite 
 
-# tailwind.config.js
-#   content: [
-#    "./index.html",
-#    "./src/**/*.tsx",
-#  ],
-#
-#  index.css
-#
-# @tailwind base;
-# @tailwind components;
-# @tailwind utilities;
-#
-# Package.json
-#
-# "tailwind": "npx tailwindcss -i ./src/stylesheets/index.css -o ./src/stylesheets/output.css --watch",
+sed -i '/"scripts": {/a\  "tailwind": "npx tailwindcss -i ./src/stylesheets/index.css -o ./src/stylesheets/output.css --watch",' package.json
+
+npm pkg fix
+
+if [ -f "./src/stylesheets/index.css" ] ; then
+  sed -i '1s|^|@import "tailwindcss";\n\n|' ./src/stylesheets/index.css
+fi
+
+printf "Vite config\nimport tailwindcss from '@tailwindcss/vite'\nplugins: [tailwindcss()]\n\n"
+printf 'index.html\n<link rel="stylesheet" href="./src/stylesheets/output.css" />\n'
